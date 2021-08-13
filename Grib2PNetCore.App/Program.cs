c50a56c0cf2891e99edd0d946dbc5ec7491863e8
liuhx <564030525@qq.com>
2021-08-13 16:25:28 +0800

    temp

---
+++ Grib2PNetCore.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grib2PNetCore.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Grib2PNetCore.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Grib2PNetCore.Core
+{
+    public class Class1
+    {
+    }
+}
